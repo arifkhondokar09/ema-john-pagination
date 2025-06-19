79fc518a5df510708aff200fafce3f79f1c14baa
md arifur rahman <aimless.arif09@gmail.com>
2025-06-19 11:23:14 +0600

    Initial commit

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -20,7 +20,7 @@ const Shop = () => {
         const savedCart = [];
         // step 1: get id of the addedProduct
         for (const id in storedCart) {
-            // step 2: get product from products state by using id
+    // step 2: get product from products state by using id
             const addedProduct = products.find(product => product._id === id)
             if (addedProduct) {
                 // step 3: add quantity
